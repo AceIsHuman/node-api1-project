c32cc9085f40da3c4509ba1d221653553a6480c0
AceIsHuman <menoman1995@yahoo.com>
2019-10-30 12:05:29 -0500

    use express to create server

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // implement your API here
+
+const express = require("express");
+
+const server = express();
+
+server.listen(4000, () => {
+  console.log("==== listening on port 4000 ====");
+});
