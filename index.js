05fdc7407e51bcaa8859621a82d0023df465a495
AceIsHuman <menoman1995@yahoo.com>
2019-10-30 12:10:17 -0500

    create post request and error if name or bio are missing

--- index.js
+++ index.js
@@ -7,3 +7,12 @@ const server = express();
 server.listen(4000, () => {
   console.log("==== listening on port 4000 ====");
 });
+
+server.post("/api/users", (req, res) => {
+  const { name, bio } = req.body;
+  if (!name || !bio) {
+    res
+      .status(400)
+      .json({ errorMessage: "Please provide name and bio for the user." });
+  }
+});
