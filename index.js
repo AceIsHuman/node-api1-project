a78d5dac23b6a613e379b98767699c3763e11684
AceIsHuman <menoman1995@yahoo.com>
2019-10-30 21:57:57 -0500

    move db.update() inside else statement

--- index.js
+++ index.js
@@ -85,18 +85,19 @@ server.put("/api/users/:id", (req, res) => {
     res
       .status(400)
       .json({ errorMessage: "Please provide name and bio for the user." });
+  } else{
+    db.update(id, req.body)
+      .then(user => {
+        if (user) {
+          res.status(200).json({ user });
+        } else {
+          res
+            .status(404)
+            .json({ message: "The user with the specified ID does not exist." });
+        }
+      })
+      .catch(err => {
+        res.status(500).json({ error: "The user information could not be modified." });
+      });
   }
-  db.update(id, req.body)
-    .then(user => {
-      if (user) {
-        res.status(201).json({ user });
-      } else {
-        res
-          .status(404)
-          .json({ message: "The user with the specified ID does not exist." });
-      }
-    })
-    .catch(err => {
-      res.status(500).json({ error: "The user information could not be modified." });
-    });
 });
